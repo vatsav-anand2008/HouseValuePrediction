73837f0f1e50c9a7d3baad4a27b89a766787b0c6
vatsav-anand2008 <vatsav.anand2008@gmail.com>
2026-07-25 19:40:33 -0700

    Update randomforesttest.py

--- notebooks+tests/randomforesttest.py
+++ notebooks+tests/randomforesttest.py
@@ -4,7 +4,7 @@ from sklearn.metrics import r2_score
 import numpy as np
 import pandas as pd
 
-housedf=pd.read_csv('housing_with_features.csv').dropna()
+housedf=pd.read_csv('../data/housing_with_features.csv').dropna()
 
 X = housedf.drop("median_house_value", axis=1)
 y = housedf["median_house_value"]
